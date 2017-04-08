8e709a69ffa5faa94da6d1fff511e4330e47eb62
Christian Javellana <cjjavellana@gmail.com>
2017-04-08 19:53:00 +0800

    Added s3 client, and sample upload files

--- ruby-sdk-samples/file_upload_to_s3.rb
+++ ruby-sdk-samples/file_upload_to_s3.rb
@@ -1,2 +1,23 @@
-class FileUploadToS3
-end
+require 'aws-sdk'
+require 'byebug'
+require './s3'
+
+### Setup Environment
+bucket_name = "s3-file-upload-temp-repo"
+s3 = Aws::S3::Client.new({region: 'ap-southeast-1'})
+
+### Connect to s3
+s3client = S3.new(s3)
+
+# Check if bucket exists
+bucket_exists = s3client.bucket_exists bucket_name
+
+# Create if missing
+s3client.create_bucket bucket_name unless bucket_exists
+
+### Perform file upload here
+s3client.upload_local_file './CIS_Red_Hat_Enterprise_Linux_6_Benchmark_v1.2.0.pdf',bucket_name
+
+## Clean up
+#s3client.delete_bucket bucket_name
+
---
+++ ruby-sdk-samples/s3.rb
@@ -0,0 +1,35 @@
+class S3 
+
+  def initialize(s3client)
+    @client = s3client
+  end
+
+  def bucket_exists(bucket_name)
+    begin
+      @client.head_bucket({:bucket => bucket_name}).on_success { |resp| return true }
+    rescue StandardError => error
+      return false
+    end
+  end
+
+  def create_bucket(bucket_name)
+    puts "Creating bucket #{bucket_name}"
+    @client.create_bucket({
+        bucket: bucket_name,
+        create_bucket_configuration: {location_constraint: "ap-southeast-1"}
+      })
+  end
+
+  def delete_bucket(bucket_name)
+    puts "Deleting bucket #{bucket_name}"
+    @client.delete_bucket({:bucket => bucket_name})
+  end
+
+  def upload_local_file(file_path, bucket_name)
+    puts "Uploading #{file_path} to #{bucket_name}"
+
+    File.open(file_path, 'rb') do |file|
+      @client.put_object(bucket: bucket_name, key: 'object-key', body: file)
+    end
+  end
+end
