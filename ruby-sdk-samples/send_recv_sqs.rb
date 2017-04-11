96efe3b6f8ebfdcba41435b5da4df43cd3274258
Christian Javellana <cjjavellana@gmail.com>
2017-04-11 19:33:40 +0800

    Added send and receive sqs message example

--- ruby-sdk-samples/send_recv_sqs.rb
+++ ruby-sdk-samples/send_recv_sqs.rb
@@ -2,9 +2,39 @@ require 'aws-sdk'
 require 'byebug'
 require './sqs'
 
+def receive_message(client, queue_url)
+  while true
+    puts "Checking if message has arrived"
+    msgs = client.receive_message queue_url#, {wait_time_seconds: 20}
+    unless msgs.messages.empty?
+      msgs.messages.each do |msg|
+        puts "Receipt Handle: #{msg.receipt_handle}; Message Content: #{msg.body}"
+        client.delete_message(queue_url, msg.receipt_handle)
+      end
+      break
+    end
+  end
+end
+
+sample_queue =  "my-sample-queue"
+
 sqs = Aws::SQS::Client.new({region: 'ap-southeast-1'})
 sqsClient = SQS.new sqs
 
-sample_queue_exists = sqsClient.queue_exists? "my-sample-queue"
+sample_queue_exists = sqsClient.queue_exists? sample_queue
 puts "Checking if my-sample-queue exists: #{sample_queue_exists}"
-sqsClient.create_queue("my-sample-queue") unless sample_queue_exists
+sqsClient.create_queue(sample_queue) unless sample_queue_exists
+
+queue_url = sqsClient.get_queue_url sample_queue
+t1 = Thread.new {
+  receive_message(sqsClient, queue_url)
+}
+
+# Wait for 5 sec before sending the message
+sleep 5
+
+# Send the message, causing the listener thread to terminate
+sqsClient.send_string_message(queue_url, "This is the message body")
+
+# Wait for listener thread to finish before we complete too
+t1.join
--- ruby-sdk-samples/sqs.rb
+++ ruby-sdk-samples/sqs.rb
@@ -4,6 +4,12 @@ class SQS
     ReceiveMessageWaitTimeSeconds: "20"
   }
 
+  RECV_MESSAGE_DEFAULT_OPS = {
+      max_number_of_messages: 1,
+      visibility_timeout: 1,
+      wait_time_seconds: 1
+  }
+
   def initialize(sqsClient)
     @sqsClient = sqsClient
   end
@@ -24,6 +30,42 @@ class SQS
     @sqsClient.create_queue({
       queue_name: queue_name,
       attributes: opts
-      })
+    })
+  end
+
+  def receive_message(queue_url, attributes = {}) 
+    opts = RECV_MESSAGE_DEFAULT_OPS.merge(attributes)
+    opts[:queue_url] = queue_url
+    @sqsClient.receive_message(opts)
+  end
+
+  def delete_message(queue_url, receipt_handle)
+    @sqsClient.delete_message({
+      queue_url: queue_url,
+      receipt_handle: receipt_handle
+    })
+  end
+
+  def get_queue_url(queue_name)
+    queue = @sqsClient.get_queue_url({
+      queue_name: queue_name
+    })
+
+    raise "Unable to find queue: #{queue_name}" if queue.nil?
+
+    queue.queue_url
+  end
+
+  def send_string_message(queue_url, message_body)
+    @sqsClient.send_message({
+      queue_url: queue_url,
+      message_body: message_body,
+      message_attributes: {
+        "RequestId" => {
+          string_value: "0xF00BAA",
+          data_type: "String"
+        }
+      }
+    })
   end
 end
